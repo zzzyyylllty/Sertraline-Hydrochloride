c41b416a1b44557da75c1db4f0d7c278dd119908
zzzyyylllty <3631901756@qq.com>
2025-02-24 23:08:08 +0800

    初步添加 chemdah 连接

--- src/main/kotlin/io/github/zzzyyylllty/connect/chemdah/quest/IDepazItemAction.kt
+++ src/main/kotlin/io/github/zzzyyylllty/connect/chemdah/quest/IDepazItemAction.kt
@@ -1,45 +1,36 @@
 package io.github.zzzyyylllty.connect.chemdah.quest
 
+import DepazItemActionEvent
+import ink.ptms.chemdah.core.PlayerProfile
+import ink.ptms.chemdah.core.quest.Task
 import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
+import ink.ptms.chemdah.core.quest.objective.other.IPlayerInventory.handler
+import org.bukkit.entity.Player
 
-object IDepazItemAction : ObjectiveCountableI() {
+object IItemPick : ObjectiveCountableI<DepazItemActionEvent>() {
 
-    /**
-     * 统一识别名称
-     */
-    override val name = "depaz action"
-
-    /**
-     * Bukkit 事件
-     */
-    override val event = EntityPickupItemEvent::class
+    override val name = "pickup item"
+    override val event = DepazItemActionEvent::class.java
 
     init {
         handler {
-            // 返回 EntityPickupItemEvent 事件下的玩家对象
-            entity as? Player
+            it.entity as? Player
         }
-        // 注册一个条件，判断玩家所在位置
-        addCondition("position") { e ->
-            toPosition().inside(e.entity.location)
+        addSimpleCondition("position") { data, e ->
+            data.toPosition().inside(e.entity.location)
         }
-        // 注册一个条件，判断玩家捡起的物品
-        addCondition("item") { e ->
-            toInferItem().isItem(e.item.itemStack)
+        addSimpleCondition("item") { data, e ->
+            data.toInferItem().isItem(e.item.itemStack)
         }
-        // 注册一个条件，判断玩家捡起的物品的数量
-        addCondition("amount") { e ->
-            toInt() <= e.item.itemStack.amount
+        addSimpleCondition("amount") { data, e ->
+            data.toInt() <= e.item.itemStack.amount
         }
-        // 将捡起的物品的数量作为变量传入脚本代理，提高操作上限
         addConditionVariable("amount") {
             it.item.itemStack.amount
         }
     }
 
-    /**
-     * 以捡起的物品数量为计数单位，并非捡起的次数。
-     */
-    override fun getCount(profile: PlayerProfile, task: Task, event: EntityPickupItemEvent): Int {
+    override fun getCount(profile: PlayerProfile, task: Task, event: DepazItemActionEvent): Int {
         return event.item.itemStack.amount
     }
+}
