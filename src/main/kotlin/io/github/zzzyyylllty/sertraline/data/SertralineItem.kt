e6002b51945506e47c4e1374f0dc4a464edfdee4
zzzyyylllty <3631901756@qq.com>
2025-07-09 22:19:18 +0800

    major fix

--- src/main/kotlin/io/github/zzzyyylllty/sertraline/data/SertralineItem.kt
+++ src/main/kotlin/io/github/zzzyyylllty/sertraline/data/SertralineItem.kt
@@ -2,29 +2,28 @@ package io.github.zzzyyylllty.sertraline.data
 
 import io.github.zzzyyylllty.sertraline.function.sertralize.AnySerializer
 import kotlinx.serialization.Serializable
-import org.bukkit.Material
 
 @Serializable
 data class SertralineItem(
     val minecraftItem: SertralineMaterial,
     val sertralineMeta: SertralineMeta,
+    val customMeta: LinkedHashMap<String, @Serializable(AnySerializer::class) Any?>,
 ) {
 }
 
 @Serializable
 data class SertralineMeta(
     val key: Key,
-    val parent: Key,
-    val data: LinkedHashMap<String, @Serializable(AnySerializer::class) Any>,
-    val customMeta: LinkedHashMap<Key, @Serializable(AnySerializer::class) Any>,
+    val parent: Key?,
+    val data: LinkedHashMap<String, @Serializable(with = AnySerializer::class) Any?>,
 )
 
 @Serializable
 data class SertralineMaterial(
-    val material: String,
-    val displayName: String,
+    val material: String?,
+    val displayName: String?,
     val lore: List<String>,
-    val nbt: LinkedHashMap<String, @Serializable(AnySerializer::class) Any>
+    val nbt: LinkedHashMap<String, @Serializable(AnySerializer::class) Any?>
 )
 
 @Serializable
--- src/main/kotlin/io/github/zzzyyylllty/sertraline/load/loadItem.kt
+++ src/main/kotlin/io/github/zzzyyylllty/sertraline/load/loadItem.kt
@@ -4,6 +4,8 @@ import io.github.zzzyyylllty.sertraline.data.Key
 import io.github.zzzyyylllty.sertraline.data.SertralineItem
 import io.github.zzzyyylllty.sertraline.data.SertralineMaterial
 import io.github.zzzyyylllty.sertraline.data.SertralineMeta
+import io.github.zzzyyylllty.sertraline.function.sertralize.AnySerializer
+import kotlinx.serialization.Serializable
 import org.bukkit.configuration.file.YamlConfiguration
 
 fun loadItem(iconfig: YamlConfiguration, root: String) : SertralineItem {
@@ -13,21 +15,26 @@ fun loadItem(iconfig: YamlConfiguration, root: String) : SertralineItem {
         keys[0], keys[1]
     ) else throw NullPointerException("The item must have a namespace, example: chotenpack:test")
 
-    val config = iconfig.getConfigurationSection(root)
+    val config = iconfig.getConfigurationSection(root)!!
+
+    var customMeta = iconfig.get(root)!! as kotlin.collections.LinkedHashMap<String, @Serializable(AnySerializer::class) Any?>
+
+    customMeta.remove("minecraft")
+    customMeta.remove("sertraline")
 
     val item = SertralineMaterial(
-        material = config.get("minecraft.material"),
-        displayName = config.get("minecraft.display-name"),
-        lore = config.get("minecraft.lore"),
-        nbt = config.get("minecraft.nbt")
+        material = config.getString("minecraft.material"),
+        displayName = config.getString("minecraft.display-name") ,
+        lore = serializeStringList(config.get("minecraft.lore")),
+        nbt = config.get("minecraft.nbt") as kotlin.collections.LinkedHashMap<String, @Serializable(AnySerializer::class) Any?>
     )
     return SertralineItem(
         minecraftItem = item,
         sertralineMeta = SertralineMeta(
-            key = TODO(),
-            parent = TODO(),
-            data = TODO(),
-            customMeta = TODO()
-        )
+            key = keyEntry,
+            parent = config.getString("sertraline.parent")?.getKey(),
+            data = config.get("sertraline.data") as kotlin.collections.LinkedHashMap<String, @Serializable(AnySerializer::class) Any?>,
+        ),
+        customMeta = customMeta
     )
 }
